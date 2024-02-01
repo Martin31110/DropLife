c87d32bf87a3cda2c1e4139cf842f7a8a4ceca7e
Abrhama Monroy <abrahamkoala919@gmail.com>
2024-01-31 19:57:46 -0600

    feat: passport setup rutas google+
    falta agregar al front el boton y que la redireccion sea a la ruta, testear el codigo.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,6 +7,7 @@ import {
 } from "../controllers/auth.controlers.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
+const passport = require('passport');
 
 const router = Router();
 
@@ -14,5 +15,10 @@ router.post("/register", validateSchema(registerSchema), register);
 router.post("/login", validateSchema(loginSchema), login);
 router.get("/verify", verifyToken);
 router.post("/logout", verifyToken, logout);
+// auth with google+
+router.get('/google', passport.authenticate('google', {
+  scope: ['profile']
+}));
+
 
 export default router;
